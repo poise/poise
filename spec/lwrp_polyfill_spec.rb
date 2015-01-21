ec209a0c5922ab5158dd1a4aded797ddfd636dea
Noah Kantrowitz <noah@coderanger.net>
2015-01-20 23:19:50 -0800

    100% coverage for LWRPPolyfill.

--- spec/lwrp_polyfill_spec.rb
+++ spec/lwrp_polyfill_spec.rb
@@ -17,49 +17,106 @@
 require 'spec_helper'
 
 describe Poise::Resource::LWRPPolyfill do
-  resource(:poise_test) do
-    include Poise::Resource::LWRPPolyfill
-    attribute(:boolean, equal_to: [true, false])
-  end
-
-  provider(:poise_test)
-
-  context 'with default value' do
-    recipe do
-      poise_test 'nil'
+  describe '#attribute' do
+    resource(:poise_test) do
+      include Poise::Resource::LWRPPolyfill
+      attribute(:boolean, equal_to: [true, false])
     end
 
-    it { is_expected.to run_poise_test('nil').with(boolean: nil) }
+    provider(:poise_test)
+
+    context 'with default value' do
+      recipe do
+        poise_test 'nil'
+      end
+
+      it { is_expected.to run_poise_test('nil').with(boolean: nil) }
+    end
+
+    context 'with true value' do
+      recipe do
+        poise_test 'true' do
+          boolean true
+        end
+      end
+
+      it { is_expected.to run_poise_test('true').with(boolean: true) }
+    end
+
+    context 'with false value' do
+      recipe do
+        poise_test 'false' do
+          boolean false
+        end
+      end
+
+      it { is_expected.to run_poise_test('false').with(boolean: false) }
+    end
+
+    context 'with string value' do
+      recipe do
+        poise_test 'true' do
+          boolean 'boom'
+        end
+      end
+
+      it { expect { subject }.to raise_error Chef::Exceptions::ValidationFailed }
+    end
+  end # /describe #attribute
+
+  describe '#default_action' do
+    resource(:poise_test) do
+      include Poise::Resource::LWRPPolyfill
+      default_action(:one)
+      actions(:two)
+    end
+    provider(:poise_test) do
+      def action_one; end
+      def action_two; end
+    end
+    recipe do
+      poise_test 'test'
+    end
+
+    it { is_expected.to one_poise_test('test') }
+  end # /describe #default_action
+end
+
+describe Poise::Provider::LWRPPolyfill do
+  describe 'load_current_resource override' do
+    subject { Chef::Provider::PoiseTest.new(nil, nil).load_current_resource }
+
+    context 'with a direct Provider subclass' do
+      provider(:poise_test, auto: false) do
+        include Poise::Provider::LWRPPolyfill
+      end
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'with an intermediary class' do
+      provider(:poise_test, auto: false, parent: Chef::Provider::RubyBlock) do
+        include Poise::Provider::LWRPPolyfill
+      end
+
+      # Chef::Provider::RubyBlock#load_current_resource returns true, use that to check if we hit an override
+      it { is_expected.to be_truthy }
+    end
   end
 
-  context 'with true value' do
-    recipe do
-      poise_test 'true' do
-        boolean true
+  describe 'Chef::DSL::Recipe include' do
+    resource(:poise_test)
+    provider(:poise_test) do
+      include Poise::Provider::LWRPPolyfill
+
+      def action_run
+        ruby_block 'test'
       end
     end
-
-    it { is_expected.to run_poise_test('true').with(boolean: true) }
-  end
-
-  context 'with false value' do
     recipe do
-      poise_test 'false' do
-        boolean false
-      end
+      poise_test 'test'
     end
 
-    it { is_expected.to run_poise_test('false').with(boolean: false) }
+    it { is_expected.to run_ruby_block('test') }
   end
-
-  context 'with string value' do
-    recipe do
-      poise_test 'true' do
-        boolean 'boom'
-      end
-    end
-
-    it { expect { subject }.to raise_error Chef::Exceptions::ValidationFailed }
-  end
-
 end
