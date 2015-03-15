11cb32b10e69bea417c93416f234d2f6ebc73a50
Noah Kantrowitz <noah@coderanger.net>
2015-03-15 16:28:19 -0700

    More tests for subresource container.

--- test/spec/subresource_container_spec.rb
+++ test/spec/subresource_container_spec.rb
@@ -18,9 +18,12 @@ require 'spec_helper'
 
 
 describe Poise::Resource::SubResourceContainer do
+  let(:chefspec_options) { {log_level: :error} } # deprecation spam
   provider(:poise_test)
   resource(:inner)
   provider(:inner)
+  # Make sure we run ruby_blocks because of the order fixed magic.
+  step_into(:ruby_block)
 
   context 'with a single subresource' do
     resource(:poise_test) do
@@ -69,20 +72,20 @@ describe Poise::Resource::SubResourceContainer do
     it { is_expected.to run_inner('name::inner') }
   end # /context with an explicit namespace
 
-  context 'with a no namespace' do
+  context 'with an explicit namespace and no inner name' do
     resource(:poise_test) do
       include Poise::Resource::SubResourceContainer
-      container_namespace(false)
+      container_namespace('name')
     end
     recipe do
       poise_test 'container' do
-        inner 'inner'
+        inner ''
       end
     end
 
     it { is_expected.to run_poise_test('container') }
-    it { is_expected.to run_inner('inner') }
-  end # /context with a no namespace
+    it { is_expected.to run_inner('name') }
+  end # /context with an explicit namespace and no inner name
 
   context 'with an explicit Proc namespace' do
     resource(:poise_test) do
@@ -98,4 +101,76 @@ describe Poise::Resource::SubResourceContainer do
     it { is_expected.to run_poise_test('container') }
     it { is_expected.to run_inner('container_name::inner') }
   end # /context with an explicit Proc namespace
+
+  context 'with a no namespace' do
+    resource(:poise_test) do
+      include Poise::Resource::SubResourceContainer
+      container_namespace(false)
+    end
+    recipe do
+      poise_test 'container' do
+        inner 'inner'
+      end
+    end
+
+    it { is_expected.to run_poise_test('container') }
+    it { is_expected.to run_inner('inner') }
+  end # /context with a no namespace
+
+  context 'with a no namespace and no inner name' do
+    resource(:poise_test) do
+      include Poise::Resource::SubResourceContainer
+      container_namespace(false)
+    end
+    recipe do
+      poise_test 'container' do
+        inner ''
+      end
+    end
+
+    it { is_expected.to run_poise_test('container') }
+    it { is_expected.to run_inner('container') }
+  end # /context with a no namespace and no inner name
+
+  describe 'resource order' do
+    resource(:poise_test) do
+      include Poise::Resource::SubResourceContainer
+    end
+    provider(:poise_test) do
+      def action_run
+        expect(node.run_state['order']).to eq 0
+        node.run_state['order'] += 1
+      end
+    end
+    resource(:inner) do
+      def val(val=nil)
+        set_or_return(:val, val, {})
+      end
+    end
+    provider(:inner) do
+      def action_run
+        expect(node.run_state['order']).to eq new_resource.val
+        node.run_state['order'] += 1
+      end
+    end
+    recipe do
+      node.run_state['order'] = 0
+      poise_test 'container' do
+        inner 'one' do
+          val 1
+        end
+        inner 'two' do
+          val 2
+        end
+      end
+    end
+
+    it do
+      # The dreaded multi-assertion test. Really I just need to force it to run,
+      # these assertions are just additional sanity.
+      is_expected.to run_poise_test('container')
+      is_expected.to run_inner('container::one')
+      is_expected.to run_inner('container::two')
+    end
+  end # /describe resource order
 end
